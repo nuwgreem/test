5a956cd8de1d812e931a540b79f63287969675a5
nuwgreem <nuwgreem@gmail.com>
2022-05-17 16:57:04 +0300

    test

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
+from fake_useragent import UserAgent
 import requests
 import json
 
+ua = UserAgent()
+print(ua.random)
+
+def collect_data():
+    response = requests.get(
+        url='https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&isStore=true&limit=60&maxPrice=10000&minPrice=1&offset=60&quality=fn&quality=mw&type=2&withStack=true',
+        headers={'user-agent': f'{ua.random}'}
+    )
 
 
-def get_date(url):
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-encoding": "gzip, deflate, br",
-        "Accept-language": "en-US,en;q=0.9,de;q=0.8,fr;q=0.7,lt;q=0.6,ru;q=0.5,sr;q=0.4,zh-CN;q=0.3,zh;q=0.2,it;q=0.1,sk;q=0.1,ca;q=0.1,es;q=0.1",
-        "Cache-control": "max-age=0",
-        "Connection": "keep-alive",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
-    }
-
-    r = requests.get(url=url, headers=headers)
-
-    with open("index.json", "w") as file:
-        json.dump(requests.json(), file, ensure_ascii=False)
+    with open('result.json', 'w') as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)
 
 def main():
-    get_date("https://cs.money/ru/")
+    collect_data()
 
 
 if __name__ == '__main__':
